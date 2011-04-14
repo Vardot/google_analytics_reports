e5fd38240d77d82ca30a11ae3e51edab97ce7ed3
Dylan Tack <grendzy@96647.no-reply.drupal.org>
2011-04-14 08:28:35 -0700

    prettify dashboard

--- reports/google_analytics_reports_summary.tpl.php
+++ reports/google_analytics_reports_summary.tpl.php
@@ -1,33 +1,52 @@
-<h3><?php print t('Visits Over the Past 30 Days'); ?></h3>
-<?php print $visit_chart; ?>
+<?php
+/**
+ * @file
+ * Theme implementation to display the Google Analytics summary.
+ */
+?>
 
-<h3><?php print t('Site Usage'); ?></h3>
-<table>
-  <tr>
-    <td><?php print $visits; ?></td>
-    <td>Visits</td>
-    <td>@TODO</td>
-    <td>Bounce Rate</td>
-  </tr>
-  <tr>
-    <td><?php print $pageviews; ?></td>
-    <td>Pageviews</td>
-    <td>@TODO</td>
-    <td>Avg. Time on Site</td>
-  </tr>
-  <tr>
-    <td>@TODO</td>
-    <td>Pages/Visit</td>
-    <td>@TODO</td>
-    <td>% New Visits</td>
-  </tr>
-</table>
+<div class="google-analytics-summary">
+  <div class="google-analytics-visits">
+    <h3><?php print t('Visits Over the Past 30 Days'); ?></h3>
+    <?php print $visit_chart; ?>
+  </div>
 
-<h3><?php print t('Top Pages'); ?></h3>
-<?php print $pages; ?>
+  <div class="google-analytics-usage">
+    <h3><?php print t('Site Usage'); ?></h3>
+    <table>
+      <tr>
+        <td><?php print $visits; ?></td>
+        <th>Visits</th>
+        <td>@TODO</td>
+        <th>Bounce Rate</th>
+      </tr>
+      <tr>
+        <td><?php print $pageviews; ?></td>
+        <th>Pageviews</th>
+        <td>@TODO</td>
+        <th>Avg. Time on Site</th>
+      </tr>
+      <tr>
+        <td>@TODO</td>
+        <th>Pages/Visit</th>
+        <td>@TODO</td>
+        <th>% New Visits</th>
+      </tr>
+    </table>
+  </div>
 
-<h3><?php print t('Top Referrals'); ?></h3>
-<?php print $referrals; ?>
+  <div class="google-analytics-pages">
+    <h3><?php print t('Top Pages'); ?></h3>
+    <?php print $pages; ?>
+  </div>
 
-<h3><?php print t('Top Searches'); ?></h3>
-<?php print $searches; ?>
+  <div class="google-analytics-referrals">
+    <h3><?php print t('Top Referrals'); ?></h3>
+    <?php print $referrals; ?>
+  </div>
+
+  <div class="google-analytics-searches">
+    <h3><?php print t('Top Searches'); ?></h3>
+    <?php print $searches; ?>
+  </div>
+</div>
